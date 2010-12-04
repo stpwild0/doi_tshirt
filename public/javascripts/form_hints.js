a960a50ff73e82b524fa697f4ded55ccf6bfd97f
kevin <kevin.lin.p@gmail.com>
2010-12-04 00:21:54 -0500

    form hints in!

---
+++ public/javascripts/form_hints.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){ 
+
+	$('.full_name').tbHinter({
+		text: 'full name',
+		class: 'greyTextItalics'
+	});
+
+	$('.department').tbHinter({
+		text: 'department name',
+		class: 'greyTextItalics'
+	});
+  
+	$('.email_address').tbHinter({
+	  text: 'vt.edu email address',
+		class: 'greyTextItalics'
+	});
+});
